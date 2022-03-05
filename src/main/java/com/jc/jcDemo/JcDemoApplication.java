17b336be0844e1bcf60ed967669fe911da99a34b
jon-cundiff <jon.cundiff@gmail.com>
2022-03-04 22:26:10 -0600

    Initial Commit

---
+++ src/main/java/com/jc/jcDemo/JcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jc.jcDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/jcDemo/JcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.jcDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
